1cef1ff8bed799ea48c339f726642aa881119d2f
tjdgus903 <tjdgusgus123@gmail.com>
2025-07-23 07:03:59 +0900

    EntityManager CRUD

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -10,7 +10,7 @@ public class JpaMain {
     public static void main(String[] args) {
         // persistence에 설정된 정보를 가져와 실행
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        // db 작업이 필요할 때 트랜잭션을 수행
+        // 엔티티 메니저 - 영속성 컨텍스트를 관리하는 주체(db 작업이 필요할 때 트랜잭션을 수행)
         EntityManager em = emf.createEntityManager();
 
         // 트랜잭션 시작
@@ -18,34 +18,25 @@ public class JpaMain {
         tx.begin();
 
         try{
-            /* 조회 */
-            Member findMember = em.find(Member.class, 2L);
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());
 
-            /* 등록
+            // 비영속
             Member member = new Member();
-            member.setId(2L);
-            member.setName("HelloB");
+            member.setId(102L);
+            member.setName("HelloJPA");
+
+
+            System.out.println("=== BEFORE ===");
+            // 영속 - entityManager를 통해 엔티티를 영속성 컨텍스트에 추가
             em.persist(member);
-            */
-            /* 삭제
-            Member findMember = em.find(Member.class, 2L);
-            em.remove(findMember);
-            */
-            /* 수정
-            Member findMember = em.find(Member.class, 2L);
-            findMember.setName("HelloJPA");
-            */
+            // 영속성 컨텍스트에서 지우기
+            // em.detach(member);
+            // 객체 삭제(delete 문)
+            // em.remove(member);
+            System.out.println("=== AFTER ===");
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
-
-            for(Member member : result){
-                System.out.println("member.name = " + member.getName());
-            }
+            Member findMember = em.find(Member.class, 102L);
+            System.out.println("findMember.id = "+findMember.getId());
+            System.out.println("findMember.name = "+findMember.getName());
 
             // 트랜잭션 커밋(변경사항 적용)
             tx.commit();
@@ -55,7 +46,7 @@ public class JpaMain {
             // 트랜잭션 닫기
             em.close();
         }
-        
+
         // 리소스 정리
         emf.close();
     }
