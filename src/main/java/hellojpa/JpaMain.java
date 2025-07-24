7bcf19c160db9d7ee2044ecb0a3908dfe2d402fd
tjdgus903 <tjdgusgus123@gmail.com>
2025-07-24 21:39:24 +0900

    settings index && sequence

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,15 +19,27 @@ public class JpaMain {
 
         try{
 
-            Member member = new Member();
-            member.setId(3L);
-            member.setUsername("C");
-            member.setRoleType(RoleType.GUEST);
+            Member member1 = new Member();
+            member1.setUsername("A");
 
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("B");
 
-            System.out.println("================");
-            // 트랜잭션 커밋(변경사항 적용)
+            Member member3 = new Member();
+            member3.setUsername("C");
+
+            System.out.println("==================");
+
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+
+            System.out.println("member1 = "+member1.getId());
+            System.out.println("member2 = "+member2.getId());
+            System.out.println("member3 = "+member3.getId());
+
+            System.out.println("==================");
+
             tx.commit();
         } catch(Exception e){
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,43 +2,29 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.Locale;
 
 // Entity를 선언하면 jpa 가 관리하는 객체로 선언이 됨(name은 class name이 기본값(변경할 일이 거의 없음))
 @Entity
+@TableGenerator(
+        name="MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        initialValue = 1, allocationSize = 50
+)
 public class Member {
 
     @Id
+    // IDENTITY : 기본키를 db에 위임
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
-    @Column(name = "name", insertable = true, updatable = true, nullable = true, unique = false, length = 10, columnDefinition = "varchar(100) default 'EMPTY'")
+    @Column(name="name", nullable = false)
     private String username;
-
-    private Integer age;
-
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    private LocalDate test1;
-    private LocalDateTime test2;
-
-
-    @Lob
-    private String description;
 
     public Member(){
 
     }
-
 
     public Long getId() {
         return id;
@@ -54,45 +40,5 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }
