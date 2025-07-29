660167e60f2cd9f66d41ccf63e401543a24bf383
tjdgus903 <tjdgusgus123@gmail.com>
2025-07-29 22:39:43 +0900

    값 타입 매핑

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 public class JpaMain {
 
@@ -19,20 +20,54 @@ public class JpaMain {
 
         try {
 
-            // embeddable 은 불변객체이기 때문에 값을 변경하지 않아야 됨
-            Address address = new Address("city", "street", "10000");
-
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(address);
+            member.setHomeAddress(new Address("homeCity", "street", "1"));
+
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressHistory().add(new AddressEntity("old1", "street", "1"));
+            member.getAddressHistory().add(new AddressEntity("old2", "street", "1"));
+
             em.persist(member);
 
-            Address newAddress = new Address("NewCity", address.getStreet(), address.getZipCode());
+            em.flush();
+            em.clear();
+/*
 
-            member.setHomeAddress(newAddress);
+            // 조회
+            Member findMember = em.find(Member.class, member.getId());
 
-            // 안됨!
-            member.getHomeAddress().setCity("newCity");
+            List<Address> addressHistory = findMember.getAddressHistory();
+            for (Address address : addressHistory) {
+                System.out.println("address = "+address.getCity());
+            }
+
+            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+            for (String favoriteFood : favoriteFoods) {
+                System.out.println("favoriteFood = "+favoriteFood);
+            }
+
+            // 수정
+            // 이렇게 하면 절대 안됨!!
+            // findMember.getHomeAddress().setCity("newCity");
+
+            // embedded 타입은 객체에 참조하는 역할이기 때문에 부분 수정이 안됨
+            // 무조건 통으로 갈아 끼워서 수정해야됨!!
+            Address a = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("newCity",a.getStreet(), a.getZipCode()));
+
+            // String 값타입은 삭제를 하고 갈아끼워야됨!
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("국밥");
+
+            // 삭제가 일어나면 데이터를 모두 삭제한 뒤 남아있는 데이터들을 insert 함
+            //
+            findMember.getAddressHistory().remove(new AddressEntity("old1", "street", "1"));
+            findMember.getAddressHistory().add(new AddressEntity("newCity1", "street", "10000"));
+*/
 
             tx.commit();
         }catch(Exception e){
--- src/main/java/jpabook/jpashop/Member.java
+++ src/main/java/jpabook/jpashop/Member.java
@@ -1,10 +1,16 @@
 package jpabook.jpashop;
 
+import net.bytebuddy.agent.builder.AgentBuilder;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import static java.util.logging.Level.ALL;
 
 @Entity
 public class Member {
@@ -22,8 +28,29 @@ public class Member {
     private Period workPeriod;
 
     // 주소
+    // 이렇게 Embedded 타입이 설정되어있으면
+    // create table 할 때 컬럼이 추가됨
     @Embedded
     private Address homeAddress;
+
+    @ElementCollection // 기본값 지연로딩(LAZY)
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+        @JoinColumn(name="MEMBER_ID")
+    )
+    @Column(name="FOOD_NAME")   // 값 타입 컬랙션을 설정할 때 리스트가 String이나 Int 같은 변환자일때는 컬럼명을 지정
+    private Set<String> favoriteFoods = new HashSet<>();
+
+/*    @ElementCollection
+    @CollectionTable(name="address", joinColumns =
+        @JoinColumn(name="MEMBER_ID")
+    ) // Address 로 설정되어있기 때문에 따로 컬럼명 지정 안해도 됨
+    private List<Address> addressHistory = new ArrayList<>();*/
+
+    // 주로 위처럼 값타입 컬랙션은[리스트인거같음] 잘 안쓰고
+    // 대부분 일대다 매핑으로 씀(실무에서 잘 안씀)
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name="MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -55,5 +82,21 @@ public class Member {
 
     public void setHomeAddress(Address homeAddress) {
         this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
